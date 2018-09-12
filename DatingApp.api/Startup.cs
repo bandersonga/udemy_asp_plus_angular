4a7d33ed7375d10ffe97fed4b8426c1b4845b99e
Brian Anderson <bandersonga@gmail.com>
2018-09-11 18:50:24 -0700

    messed up spelling DateOfBirth in User Model. Gonna try to drop the database after this and rerun migrations

--- DatingApp.api/Startup.cs
+++ DatingApp.api/Startup.cs
@@ -19,6 +19,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using DatingApp.api.Helpers;
+using AutoMapper;
+
+//Note 1:   The server jacks up because our User Model class and Photos Model class reference each other (so it thinks its a circular loop)
+//          Adding this line basically tells MVC to ignore this 'error'
 
 namespace DatingApp.api
 {
@@ -35,9 +39,15 @@ namespace DatingApp.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore; // See note 1 up top
+                });
             services.AddCors();
+            services.AddAutoMapper();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
@@ -51,7 +61,7 @@ namespace DatingApp.api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -76,6 +86,7 @@ namespace DatingApp.api
             }
 
             // app.UseHttpsRedirection();
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
